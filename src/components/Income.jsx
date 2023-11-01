aada8bc50176372d93f6c887a9a2f1fc017479e1
rohanasif1990 <rohanasif1990@gmail.com>
2023-11-01 20:49:25 +0500

    remove reactchart-2

--- src/components/Income.jsx
+++ src/components/Income.jsx
@@ -4,9 +4,10 @@ const Income = () => {
   const incomes = useSelector((state) => state.incomes);
   const dispatch = useDispatch();
   return (
-    <div>
+    <div className="bg-white">
       <h1>Income</h1>
       <p>Rs. {incomes.reduce((total, income) => total + income.quantity, 0)}</p>
+      
     </div>
   );
 };
